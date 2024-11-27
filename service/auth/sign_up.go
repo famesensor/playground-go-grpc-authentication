69c2b7b4677346e6928137f8a8f7f1310a4a6d99
famesensor <sathit.ju@outlook.com>
2024-11-28 00:18:53 +0700

    feat: add check username duplicate

--- service/auth/sign_up.go
+++ service/auth/sign_up.go
@@ -2,12 +2,18 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/famesensor/playground-go-grpc-authentication/model"
 )
 
 func (s *service) SignUp(ctx context.Context, req *model.SignUpReq) error {
+	_, err := s.database.GetByUsername(ctx, req.Username)
+	if err == nil {
+		return errors.New("username already exists")
+	}
+
 	if err := s.database.Create(ctx, &model.CreateUserReq{
 		Username:  req.Username,
 		Password:  req.Password,
